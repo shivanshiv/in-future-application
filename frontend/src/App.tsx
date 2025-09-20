8795b0b25a26b137761c6f0ec5e26b14a85355e4
shivanshiv <shivanshi.visvanatha@ucalgary.ca>
2025-09-19 22:18:47 -0600

    Uploaded current website

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Problems from './components/Problems';
 import Workflow from './components/Workflow';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import ApplicationsList from './components/ApplicationsList';
 
 export default function App() {
 	return (
@@ -13,6 +14,7 @@ export default function App() {
 				<Hero />
 				<Problems />
 				<Workflow />
+				<ApplicationsList />
 				<Contact />
 			</main>
 			<Footer />
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,12 +6,15 @@ import App from './App';
 import BuildWebsite from './pages/BuildWebsite';
 import FindInternship from './pages/FindInternship';
 import Login from './pages/Login';
+import ApplicationsList from './components/ApplicationsList';
+
 
 const router = createBrowserRouter([
 	{ path: '/', element: <App /> },
 	{ path: '/build-website', element: <BuildWebsite /> },
 	{ path: '/find-internship', element: <FindInternship /> },
 	{ path: '/login', element: <Login /> },
+	{ path: '/application', element: <ApplicationsList /> },
 ]);
 
 const root = createRoot(document.getElementById('root')!);
